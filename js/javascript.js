91a49d182f525f57bb925525bff5fd96110fb40a
Gabriel <89097408+Quaresmasr@users.noreply.github.com>
2023-09-13 18:08:42 -0300

    Cadastro

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,6 @@
+const form = document.getElementById('cadastro-form');
 
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    
